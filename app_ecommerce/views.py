403446ed0b14d118aa773864884e5ccad2799baa
kosdmit <kosdmit@hotmail.com>
2023-07-03 23:41:06 +0400

    Category breadcrumbs have been added to ecommerce page

--- app_ecommerce/views.py
+++ app_ecommerce/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView
 
 from app_ecommerce.models import Goods, Category
@@ -11,22 +12,48 @@ class GoodsView(ListView):
     model = Goods
     paginate_by = 9
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.category_object = None
+
+    def dispatch(self, request, *args, **kwargs):
+        category_slug = self.request.GET.get('category')
+        if category_slug:
+            try:
+                self.category_object = Category.objects.get(slug=category_slug)
+            except Category.DoesNotExist:
+                raise Http404
+
+        return super().dispatch(request, *args, **kwargs)
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         top_level_categories = Category.objects.filter(parent__isnull=True)
         context['top_level_categories'] = top_level_categories
 
+        # Get breadcrumbs
+        breadcrumbs = []
+        if self.category_object and self.category_object.parent:
+            breadcrumbs.append([
+                self.category_object.parent.title,
+                reverse_lazy('goods') + '?category=' + self.category_object.parent.slug
+            ])
+
+        if self.category_object:
+            breadcrumbs.append([self.category_object.title, ''])
+        else:
+            breadcrumbs.append(['Все категории', ''])
+
+        context['breadcrumbs'] = breadcrumbs
+
         return context
 
     def get_queryset(self):
-        category_slug = self.request.GET.get('category')
-        if category_slug:
-            try:
-                category_object = Category.objects.get(slug=category_slug)
-            except Category.DoesNotExist:
-                raise Http404
-            query_set = category_object.get_goods()
+        if self.category_object:
+            query_set = self.category_object.get_goods()
             return query_set
         else:
             return super().get_queryset()
