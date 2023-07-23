ee6c2b8b843b8039867eeab7054cf36c0d4f4618
kosdmit <kosdmit@hotmail.com>
2023-07-23 18:14:30 +0400

    Html minificator disabled

--- carkeys_project/settings.py
+++ carkeys_project/settings.py
@@ -55,8 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'htmlmin.middleware.HtmlMinifyMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
+    # 'htmlmin.middleware.HtmlMinifyMiddleware',
+    # 'htmlmin.middleware.MarkRequestMiddleware',
     # X-Frame_Option is configurated in Nginx conf
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
