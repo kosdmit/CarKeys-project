0a608a6b7a6e855d8be01db10ff664af4c667c88
kosdmit <kosdmit@hotmail.com>
2023-07-23 17:56:13 +0400

    Fix for creating customers obj process

--- app_ecommerce/mixins.py
+++ app_ecommerce/mixins.py
@@ -7,12 +7,7 @@ class AddCustomerFormMixin:
         context = super().get_context_data(**kwargs)
 
         session_id = self.request.session.session_key
-        if session_id:
-            customer = Customer.objects.filter(session_id=session_id).first()
-        else:
-            self.request.session.create()
-            session_id = self.request.session.session_key
-            customer = Customer.objects.create(session_id=session_id)
+        customer = Customer.objects.filter(session_id=session_id).first()
 
         customer_form = CustomerForm(instance=customer)
         context['customer_form'] = customer_form
