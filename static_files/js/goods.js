8c4dc90e42081d6be441fcda8e38994b48c7ed87
kosdmit <kosdmit@hotmail.com>
2024-03-10 18:49:45 +0400

    Disabled head links in Categories

--- static_files/js/goods.js
+++ static_files/js/goods.js
@@ -1,9 +1,9 @@
 // Stop propagation for category links in accordion header
-const categoryAccordionPanel = document.querySelector('#categoryAccordionPanel')
-const accordionHeaderLinks = categoryAccordionPanel.querySelectorAll('.accordion-header a')
-for (let i = 0; i < accordionHeaderLinks.length; i++) {
-  accordionHeaderLinks[i].addEventListener('click', function (event) {
-    event.stopPropagation();
-    window.location.href = accordionHeaderLinks[i].getAttribute('href')
-  });
-}
+// const categoryAccordionPanel = document.querySelector('#categoryAccordionPanel')
+// const accordionHeaderLinks = categoryAccordionPanel.querySelectorAll('.accordion-header a')
+// for (let i = 0; i < accordionHeaderLinks.length; i++) {
+//   accordionHeaderLinks[i].addEventListener('click', function (event) {
+//     event.stopPropagation();
+//     window.location.href = accordionHeaderLinks[i].getAttribute('href')
+//   });
+// }
