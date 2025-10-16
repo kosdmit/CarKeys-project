7c632a2df8b2fc0ad8bfc42bb34970613db7b55b
kosdmit <kosdmit@hotmail.com>
2025-10-16 23:30:46 +0300

    Security update

--- carkeys_project/settings.py
+++ carkeys_project/settings.py
@@ -32,24 +32,24 @@ ALLOWED_HOSTS = ['carkeysamara.ru', 'www.carkeysamara.ru',
                  '127.0.0.1', 'localhost', '95.142.42.246']
 
 # Production Security Settings
-# if not DEBUG:
-#     SECURE_SSL_REDIRECT = True
-#     SECURE_HSTS_SECONDS = 31536000  # 1 year
-#     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-#     SECURE_HSTS_PRELOAD = True
-#     SESSION_COOKIE_SECURE = True
-#     CSRF_COOKIE_SECURE = True
-#     SECURE_BROWSER_XSS_FILTER = True
-#     SECURE_CONTENT_TYPE_NOSNIFF = True
-#     X_FRAME_OPTIONS = 'SAMEORIGIN'
-#
-#     # CSRF trusted origins for production
-#     CSRF_TRUSTED_ORIGINS = [
-#         'https://carkeysamara.ru',
-#         'https://www.carkeysamara.ru',
-#         'https://carkeysamara.online',
-#         'https://www.carkeysamara.online',
-#     ]
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+    # CSRF trusted origins for production
+    CSRF_TRUSTED_ORIGINS = [
+        'https://carkeysamara.ru',
+        'https://www.carkeysamara.ru',
+        'https://carkeysamara.online',
+        'https://www.carkeysamara.online',
+    ]
 
 
 # Application definition
